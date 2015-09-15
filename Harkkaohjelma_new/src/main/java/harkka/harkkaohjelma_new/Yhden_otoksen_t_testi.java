ce2bcc3d239e1fa7535610376196bdeef086309b
tkakonen <taru.kakonen@helsinki.fi>
2015-09-15 13:26:28 +0300

    toinen versio ohjelmasta

--- Harkkaohjelma_new/src/main/java/harkka/harkkaohjelma_new/Yhden_otoksen_t_testi.java
+++ Harkkaohjelma_new/src/main/java/harkka/harkkaohjelma_new/Yhden_otoksen_t_testi.java
@@ -1,3 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package harkka.harkkaohjelma_new;
+
 import java.util.ArrayList;
 
 /*
