b87a11ae8f0624a35682268c12bfa6b79d11ffe3
tkakonen <taru.kakonen@helsinki.fi>
2015-10-02 22:38:40 +0300

    kayttoliittymaluonnostelua

---
+++ Harkkaohjelma_new/src/main/java/harkka/harkkaohjelma_new/exceptions/TyhjaMuuttujaException.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package harkka.harkkaohjelma_new.exceptions;
+
+/**
+ *
+ * @author DELL
+ */
+public class TyhjaMuuttujaException extends Exception {
+
+
+}
